d2c55d89825303a855e34971c3a507c05527773f
kkhys <kkhys@pm.me>
2026-01-24 20:42:35 +0900

    feat(nix): integrate nix-darwin and nix-homebrew
    - Add darwin and nix-homebrew inputs to flake.nix
    - Migrate from homeConfigurations to darwinConfigurations
    - Enable declarative Homebrew management via nix-homebrew
    - Add home-manager as darwin module with extraSpecialArgs
    - Update flake.lock with new dependencies
    
    This change enables system-level configuration management on macOS
    and consolidates both system and user-level settings into a single
    darwin-rebuild command.

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -3,31 +3,88 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      darwin,
+      home-manager,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+      ...
+    }:
     let
       system = "aarch64-darwin";
 
       commonModules = [
         ./modules/hostSpec.nix
-        ./home-manager
         ./hosts/common
+        ./darwin
+        home-manager.darwinModules.home-manager
+        (
+          { config, ... }:
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "backup";
+              extraSpecialArgs = {
+                hostSpec = config.hostSpec;
+              };
+              users.${config.hostSpec.username} = import ./home-manager;
+            };
+          }
+        )
+        nix-homebrew.darwinModules.nix-homebrew
+        (
+          { config, ... }:
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = config.hostSpec.username;
+              autoMigrate = true;
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+              };
+              mutableTaps = true;
+            };
+          }
+        )
       ];
-
-      mkHomeConfiguration = hostModule: home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [ hostModule ] ++ commonModules;
-      };
     in
     {
-      homeConfigurations = {
-        kkhys = mkHomeConfiguration ./hosts/kkhys;
-        work = mkHomeConfiguration ./hosts/work;
+      darwinConfigurations = {
+        kkhys = darwin.lib.darwinSystem {
+          inherit system;
+          modules = [ ./hosts/kkhys ] ++ commonModules;
+        };
+
+        work = darwin.lib.darwinSystem {
+          inherit system;
+          modules = [ ./hosts/work ] ++ commonModules;
+        };
       };
     };
 }
